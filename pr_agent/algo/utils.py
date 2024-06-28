fc8b8174f2f504616e1e7506a9b2804ed7921528
Greg Meldrum <gregmeldrum@gmail.com>
2024-06-28 17:43:55 -0400

    Hardcode max tokens

--- pr_agent/algo/utils.py
+++ pr_agent/algo/utils.py
@@ -554,7 +554,8 @@ def get_max_tokens(model):
     if model in MAX_TOKENS:
         max_tokens_model = MAX_TOKENS[model]
     else:
-        raise Exception(f"MAX_TOKENS must be set for model {model} in ./pr_agent/algo/__init__.py")
+        return 100000
+        #raise Exception(f"MAX_TOKENS must be set for model {model} in ./pr_agent/algo/__init__.py")
 
     if settings.config.max_model_tokens:
         max_tokens_model = min(settings.config.max_model_tokens, max_tokens_model)
